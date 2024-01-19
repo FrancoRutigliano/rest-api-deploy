aa13f823a3820b915c0c41f6268d450ac2c47cb4
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-19 12:02:24 -0300

    Refactorización y ordenamiento de código -Routes/-Middleware

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+import cors from 'cors'
+
+// accepted origins desde fuera
+const ACCEPTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'http://movies.com',
+    'http://midu.dev'
+]
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+    origin: (origin, callback) => {
+
+        if (acceptedOrigins.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+        return callback(new Error('Not Allowed by CORS'))
+    }
+})
