7de387f93d8308b708c3ed380837547914755fd7
FrancoRutigliano <francorutigliano11@gmail.com>
2024-01-22 14:07:17 -0300

    Modularización de código agregando controllers.

--- routes/movies.js
+++ routes/movies.js
@@ -1,60 +1,18 @@
 import { Router } from "express"
 
-
-import { validateMovie, validatePartialMovie } from './schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from "../controllers/movie.js"
 
 export const moviesRouter = Router()
 
-// cuando el router vaya a la raiz, respondemos con la siguiente funcion
-moviesRouter.get('/', async (req, res) => {
-    const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
-    res.json(movies)
-})
 
-moviesRouter.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const movie = await MovieModel.getById({ id })
-    if (movie) return res.json(movie)
-    res.status(404).json({ message: 'Movie not found' })
-})
+moviesRouter.get('/', MovieController.getAll)
+
+moviesRouter.get('/:id', MovieController.getById)
 
 // metodo post
-moviesRouter.post('/', async (req, res) => {
-    const result = validateMovie(req.body)
+moviesRouter.post('/', MovieController.create)
 
-    if (!result.success) {
-        // 422 Unprocessable Entity
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
+moviesRouter.delete('/:id', MovieController.delete)
 
-    const newMovie = await MovieModel.create({ input: result.data })
-
-    res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-    const { id } = req.params
-
-    const result = await MovieModel.delete({ id })
-    if (result === false) {
-        return res.status(404).json({ message: 'Movie not found' })
-    }
-    return res.json({ message: 'Movie deleted' })
-})
-
-moviesRouter.patch('/movies/:id', async (req, res) => {
-    const result = validatePartialMovie(req.body)
-
-    if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    const { id } = req.params
-
-    const updateMovie = await MovieModel.update({ id, input: result.data })
-
-    return res.json(updateMovie)
-})
+moviesRouter.patch('/movies/:id', MovieController.update)
 
